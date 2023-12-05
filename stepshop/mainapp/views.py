c7d5a12440a8184e25b40a9a2229172c04264b13
кайыржанулы Райымбек <@step.kz>
2023-12-05 17:40:03 +0600

    lesson_11 fiels

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from mainapp.models import Product, Category
 
 
@@ -18,29 +19,41 @@ def get_data(**kawargs):
     return context
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
+
+
 def index(request):
     title = "главное"
     prods = Product.objects.all()
-    context = get_data(title=title, prods=prods)
+    basket = get_basket(request.user)
+    context = get_data(title=title, prods=prods, basket=basket)
     return render(request, 'index.html', context)
 
 
 def about(request):
     title = "О нас"
-    context = get_data(title=title)
+    basket = get_basket(request.user)
+    context = get_data(title=title, basket=basket)
     return render(request, 'about.html', context)
 
 
 def contacts(request):
     title = "Связаться с нами"
-    context = get_data(title=title)
+    basket = get_basket(request.user)
+    context = get_data(title=title, basket=basket)
     return render(request, 'contacts.html', context)
 
 
 def product(request, pk):
     title = "Продукт"
     prod = Product.objects.get(pk=pk)
-    context = get_data(title=title, prod=prod)
+    same_prod = Product.objects.exclude(pk=pk) #filter(category=prod.category)
+    basket = get_basket(request.user)
+
+    context = get_data(title=title, prod=prod, basket=basket, same_prod=same_prod)
     return render(request, 'product.html', context)
 
 
@@ -49,11 +62,13 @@ def products(request, pk=None):
     _products = Product.objects.order_by('price')
     context = {}
 
+    basket = get_basket(request.user)
+
     if pk is not None:
         category = get_object_or_404(Category, pk=pk)
         _products = Product.objects.filter(category__pk=pk).order_by('price')
         context = get_data(category=category)
         context['selected_category_name'] = category.name
 
-    context = get_data(title=title, prods=_products, **context)
+    context = get_data(title=title, prods=_products, basket=basket, **context)
     return render(request, 'products.html', context)
