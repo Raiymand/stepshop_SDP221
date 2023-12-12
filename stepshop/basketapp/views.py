1fefbb2ae762f918019aa87a34ca7ba747935337
кайыржанулы Райымбек <@step.kz>
2023-12-12 16:39:58 +0600

    lesson 12 files

--- stepshop/basketapp/views.py
+++ stepshop/basketapp/views.py
@@ -1,20 +1,23 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from mainapp.views import get_data
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
     if request.user.is_authenticated:
         user_basket = Basket.objects.filter(user=request.user)
-        context = get_data(basket=user_basket)  # Включаем переменные links_menu и categories в контекст
+        context = get_data(basket=user_basket)
         return render(request, 'basket/basket.html', context)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     user_basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -26,9 +29,12 @@ def basket_add(request, pk):
     user_basket.quantity += 1
     user_basket.save()
 
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
-
-def basket_remove(request):
-    pass
+@login_required
+def basket_remove(request, pk):
+    basket_item = get_object_or_404(Basket, pk=pk)
+    basket_item.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
